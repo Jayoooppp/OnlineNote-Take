9b67f6d3fe0a64684af501ade30071d25f2bb252
Jayoooppp <patel.js@somaiya.edu>
2022-07-30 10:55:16 +0530

    Port number changed in server

--- server.js
+++ server.js
@@ -142,6 +142,10 @@ app.route("/note/:id")
 
 
 
-app.listen(8080 || process.env.PORT, function () { 
-    console.log("Server started")
- })
+
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+app.listen(port, host, function() {
+    console.log("Server started.......");
+});
