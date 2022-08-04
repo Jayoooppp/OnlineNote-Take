5787c9cbafe3fc743645e3c8b2d153149ac1cb14
Jayoooppp <patel.js@somaiya.edu>
2022-08-04 18:45:05 +0530

    API changes

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.post("/login", function (req, res) {
 
 
 // Signup Route
-app.post("/signup", function (req, res) {
+app.get("/signup", function (req, res) {
     let fname = req.body.fname;
     let lname = req.body.lname;
     let new_email = req.body.email;
